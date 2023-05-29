28b716a5ff9842ae7137ea30a4d03d171d05509c
thayrun <132508300+thayrun@users.noreply.github.com>
2023-05-29 16:37:05 -0300

    Terceiro Commit

---
+++ atividadesLacosCond/ExercicioIf.java
@@ -0,0 +1,37 @@
+package atividadesLacosCond;
+
+import java.util.Scanner;
+
+public class ExercicioIf {
+
+	public static void main(String[] args) {
+		
+		int numA, numB, numC, comp;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o número A: ");
+		numA = leia.nextInt();
+		
+		System.out.println("Digite o número B: ");
+		numB = leia.nextInt();
+		
+		System.out.println("Digite o número C: ");
+		numC = leia.nextInt();
+		
+		comp = numA + numB;
+		System.out.println(numA + " + " + numB + " = " + comp);
+		
+		if(comp > numC) {
+			System.out.println("A soma de A + B é MAIOR do que C.");
+		}
+		else if(comp < numC){
+			System.out.println("A soma de A + B é MENOR do que C.");
+		}
+		else{
+			System.out.println("A soma de A + B é IGUAL a C");
+		}
+
+	}
+
+}
