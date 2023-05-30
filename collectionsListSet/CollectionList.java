d523e8d747fffa3760711274a2a9a31e08473ae4
thayrun <132508300+thayrun@users.noreply.github.com>
2023-05-30 15:29:22 -0300

    collectionsListSet

---
+++ collectionsListSet/CollectionList.java
@@ -0,0 +1,33 @@
+package collectionsListSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class CollectionList {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		ArrayList<String> cores = new ArrayList<String>();
+
+		System.out.println("Digite 5 Cores Diferentes: ");
+		for (int conta = 0; conta < 5; conta++) {
+			String cor = leia.nextLine();
+			cores.add(cor);
+		}
+
+		System.out.println("\nLista das Cores Escolhidas: ");
+		for (String cor : cores) {
+			System.out.println(cor);
+		}
+
+		Collections.sort(cores);
+
+		System.out.println("\nAs Cores Escolhidas em Forma Crescente: ");
+		for (String cor : cores) {
+			System.out.println(cor);
+		}
+
+	}
+
+}
---
+++ collectionsListSet/CollectionSet.java
@@ -0,0 +1,26 @@
+package collectionsListSet;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class CollectionSet {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		Set<Integer> numeros = new HashSet<Integer>();
+		Integer lerNum;
+
+		System.out.println("Digite 10 valores inteiros NÃO repetidos: ");
+		for (int roda = 0; roda < 10; roda++) {
+			lerNum = leia.nextInt();
+			numeros.add(lerNum);
+		}
+
+		System.out.println("Listar dados do Set: ");
+
+		for (Integer num : numeros) {
+			System.out.println(num);
+		}
+	}
+}
